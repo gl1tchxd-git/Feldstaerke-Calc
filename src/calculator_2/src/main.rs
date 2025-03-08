a0388f3d446e218ab7034ac8226ec219a7d70d3a
gl1tchxd-git <contact@gl1tchxd.at>
2025-03-08 14:06:58 +0100

    should technically work, but probably doesn't ;-;

--- src/calculator_2/src/main.rs
+++ src/calculator_2/src/main.rs
@@ -1,19 +1,5 @@
-mod functions;
-use crate::functions::*;
+mod coil_calculator;
+use coil_calculator::coil::*;
 
-pub fn sigma(dl: [f64; 3], r: [f64; 3], ri: [f64; 3], sigma: &mut f64) {
-    let mut top: [f64; 3];
-    subtract_vector(&r, &ri, &mut top);
-    let mut bot_vec: [f64; 3];
-    let mut bot: f64 = 0.0;
-    subtract_vector(&r, &ri, &mut bot_vec);
-    abs_vector(&bot_vec, &mut bot);
-    bot = bot.powi(3);
-    divide_vector(&dl, bot, sigma)
-}
 
-fn main() {
-    let mut sigma_val: f64 = 0.0;
-    sigma([2.0, 2.5, 3.0], [2.0, 2.5, 3.0], [2.0, 2.5, 3.0], &mut sigma_val);
-    println!("{}", sigma_val)
-}
+fn main() {}
