a9e18d6ff0003f0bb7e16af7a797f6ca234deb11
Felix Buchsteiner <buchsteiner.felix@gmail.com>
2025-03-08 14:06:58 +0100

    should technically work, but probably doesn't ;-;

--- src/library/src/biot_savart.rs
+++ src/library/src/biot_savart.rs
@@ -1,11 +1,12 @@
 use crate::utils::*;
 
 pub fn biot_savart(dl: [f64; 3], r: [f64; 3], ri: [f64; 3], mut summands: [f64; 3]) {
-    let mut vec_diff_r: [f64; 3];
-    let mut vec_cross_right: [f64; 3];
-    let mut abs3_r: f64;
+    let mut vec_diff_r: [f64; 3] = [0.0; 3];
+    let mut vec_cross_right: [f64; 3] = [0.0; 3];
+    let mut abs3_r: f64 = 0.0;
 
     vec_diff(&r, &ri, &mut vec_diff_r);
     abs3(&vec_diff_r, &mut abs3_r);
     vec_div(&vec_diff_r, abs3_r, &mut vec_cross_right);
     cross_product(&dl, &vec_cross_right, &mut summands);
+}
--- src/library/src/solenoid.rs
+++ src/library/src/solenoid.rs
@@ -1,14 +1,34 @@
+use std::f64::consts::PI;
 use crate::biot_savart::biot_savart;
+use crate::utils::*;
 
 pub struct Solenoid {
     pub current: f64,
     pub turns: usize,
     pub length: f64,
     pub radius: f64,
+    pub wire_radius: f64,
 }
 
 impl Solenoid {
-    pub fn calculate() -> f64 {
+    pub fn calculate(&self, iterations: usize) -> f64 {
+        let mut sum = [0.0; 3];
+        for _i in 0..iterations * self.turns {
+            let mut dl = [0.0; 3];
+            let mut ri = [0.0; 3];
+            let mut dl_half = [0.0; 3];
+            let r = [0.0, self.length / 2.0, 0.0];
 
+            let mut pos_start = [0.0; 3];
+            let mut pos_end = [0.0; 3];
+
+            change_pos(&mut pos_end, self.wire_radius, self.radius, self.length, iterations, self.turns);
+            vec_diff(&pos_end, &pos_start, &mut dl);
+            vec_div(&dl, 2.0, &mut dl_half);
+            vec_add(&pos_start, &dl_half, &mut ri);
+
+            biot_savart(dl, r, ri, sum);
+        }
+        self.current / (4.0 * PI) * (sum[0].powi(2) + sum[1].powi(2) + sum[2].powi(2)).sqrt()
     }
 }
